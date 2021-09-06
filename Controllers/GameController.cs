900e7cf9e99a4221cd32d5f3b242ac6dcb300eef
Onur Baskan <baskanonurr@gmail.com>
2021-09-06 09:42:27 +0300

    first commit of the Edulu project

---
+++ Controllers/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Edulu3.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        [Route("Game/GamePage")]
+        public ActionResult GamePage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Edulu3.Global" Language="C#" %>
